01d6e71f56959fdcd4cbb543a27ef1a9b933a9a2
Alexey Mileev <alemileev@gmail.com>
2016-09-26 23:20:03 +0300

    Add default template

---
+++ templates/default/app/src/main/java/root/activity/MainActivity.java
@@ -0,0 +1,34 @@
+package ${packageName}.activity;
+
+import android.os.Bundle;
+
+import ${packageName}.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int layoutResourceId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected int toolbarTitleId() {
+        return R.string.app_name;
+    }
+
+    @Override
+    protected boolean displayHomeAsUp() {
+        return false;
+    }
+
+    @Override
+    protected boolean homeButtonEnabled() {
+        return false;
+    }
+
+}
---
+++ util/DefaultAppTemplate/app/src/main/java/com/domain/product/CustomApplication.java
@@ -0,0 +1,13 @@
+package com.domain.product;
+
+
+import android.app.Application;
+
+public class CustomApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
