08aafc5c8d781973cc63c34b7755648bb1952ad5
Alexey Mileev <alemileev@gmail.com>
2016-09-26 23:04:05 +0300

    Add some default template javadoc

--- util/DefaultAppTemplate/app/src/main/java/com/domain/product/activity/BackArrowActivity.java
+++ util/DefaultAppTemplate/app/src/main/java/com/domain/product/activity/BackArrowActivity.java
@@ -2,6 +2,12 @@ package com.domain.product.activity;
 
 import android.view.MenuItem;
 
+/**
+ * Simple base class for activities with home toolbar arrow.
+ * {@link BackArrowActivity#onBackPressed()} is invoked on the arrow click,
+ * so you can override {@link BackArrowActivity#onBackPressed()} or
+ * {@link BackArrowActivity#onOptionsItemSelected(MenuItem)} for customization.
+ */
 public abstract class BackArrowActivity extends BaseActivity {
 
     @Override
--- util/DefaultAppTemplate/app/src/main/java/com/domain/product/activity/BaseActivity.java
+++ util/DefaultAppTemplate/app/src/main/java/com/domain/product/activity/BaseActivity.java
@@ -2,6 +2,7 @@ package com.domain.product.activity;
 
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,14 +10,22 @@ import android.support.v7.widget.Toolbar;
 
 import com.domain.product.R;
 
+/**
+ * Simple base class for all activities in the app.
+ * {@link BaseActivity} finds {@link Toolbar} by R.id.toolbar id and sets it up.
+ */
 public abstract class BaseActivity extends AppCompatActivity {
 
+    @Nullable
     protected Toolbar toolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layoutResourceId());
+        int layoutId = layoutResourceId();
+        if (layoutId != 0) {
+            setContentView(layoutId);
+        }
         setUpToolbar();
     }
 
@@ -24,7 +33,10 @@ public abstract class BaseActivity extends AppCompatActivity {
         toolBar = (Toolbar) findViewById(R.id.toolbar);
         if (toolBar != null) {
             setSupportActionBar(toolBar);
-            setTitle(toolbarTitleId());
+            int titleId = toolbarTitleId();
+            if (titleId != 0) {
+                setTitle(titleId);
+            }
         }
 
         ActionBar actionBar = getSupportActionBar();
@@ -34,14 +46,26 @@ public abstract class BaseActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * @return id of your activity layout. Return 0 if you don't want to provide the layout.
+     */
     @LayoutRes
     protected abstract int layoutResourceId();
 
+    /**
+     * @return id of your toolbar title. Return 0 if you don't want to provide the title.
+     */
     @StringRes
     protected abstract int toolbarTitleId();
 
+    /**
+     * @return whether home should be displayed as an "up" affordance.
+     */
     protected abstract boolean displayHomeAsUp();
 
+    /**
+     * @return whether home button in the corner of {@link Toolbar} should be enabled
+     */
     protected abstract boolean homeButtonEnabled();
 
 }
